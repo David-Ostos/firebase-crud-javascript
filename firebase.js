e24c5f09837944c8f8613e7dbc6b0d10cdf37edc
David Ostos <davidostos2@gmail.com.com>
2023-02-01 15:21:47 -0400

    termiando page con firebase trabjando con el storage

--- firebase.js
+++ firebase.js
@@ -1,49 +1,51 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  onSnapshot,
-  addDoc,
-  deleteDoc,
-  doc,
-  getDoc,
-  updateDoc,
-} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+  import { 
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  // Put you credentials here
-};
+    getFirestore, // inicializar firebas 
+    collection, //hacer llamado a la coleccion y obtener los datos 
+    addDoc, // para agregar un id a la base de datos
+    getDocs, // para traer los datos de la base de datos (fireStore)
+    deleteDoc, // para eliminar de la base de datos (fireStore)
+    onSnapshot, // este metodo se utiliza para actualizar en tiempo real 
+    doc, // obtener datos de un solo documento 
+    getDoc, // obtener un documento
+    updateDoc, // para actualizar un documento
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore();
 
-/**
- * Save a New Task in Firestore
- * @param {string} title the title of the Task
- * @param {string} description the description of the Task
- */
-export const saveTask = (title, description) =>
-  addDoc(collection(db, "tasks"), { title, description });
+   } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js"
 
-export const onGetTasks = (callback) =>
-  onSnapshot(collection(db, "tasks"), callback);
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-/**
- *
- * @param {string} id Task ID
- */
-export const deleteTask = (id) => deleteDoc(doc(db, "tasks", id));
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD36U2HT9it_66z2NCYSCFrLVZFgI5mY-4",
+    authDomain: "fir-crud-500fc.firebaseapp.com",
+    projectId: "fir-crud-500fc",
+    storageBucket: "fir-crud-500fc.appspot.com",
+    messagingSenderId: "392338546333",
+    appId: "1:392338546333:web:2c34a02dec08e9e26bfe74"
+  };
 
-export const getTask = (id) => getDoc(doc(db, "tasks", id));
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
-export const updateTask = (id, newFields) =>
-  updateDoc(doc(db, "tasks", id), newFields);
+  const db =getFirestore();
 
-export const getTasks = () => getDocs(collection(db, "tasks"));
+    // con esta funcion agregamos al cloud 
+  export const saveTask = (title, description) => 
+    addDoc(collection(db, 'tasks') , {title, description});
+
+    //con esta funcion llamamos del cloud 
+  export const getTasks = () => getDocs(collection(db, 'tasks'));
+
+
+  export const onGetTasks = (callback) => onSnapshot(collection(db, 'tasks'), callback); 
+
+  export const deleteTasks = id => deleteDoc(doc(db, 'tasks', id));
+
+  export const getTask = id => getDoc(doc(db, 'tasks', id));
+
+  export const updateTask = (id, newFilds) => updateDoc(doc(db, 'tasks', id), newFilds);
--- index.js
+++ index.js
@@ -1,99 +1,92 @@
-import {
+import { 
+
+  saveTask, 
+  getTasks,
   onGetTasks,
-  saveTask,
-  deleteTask,
+  deleteTasks,
   getTask,
   updateTask,
-  getTasks,
-} from "./firebase.js";
 
-const taskForm = document.getElementById("task-form");
-const tasksContainer = document.getElementById("tasks-container");
+  
+  } from "./firebase.js";
+
+const tasksConrainer = document.getElementById('tasks-container');
+const taskForm = document.getElementById('task-form');
 
 let editStatus = false;
-let id = "";
+let id = '';
 
-window.addEventListener("DOMContentLoaded", async (e) => {
-  // const querySnapshot = await getTasks();
-  // querySnapshot.forEach((doc) => {
-  //   console.log(doc.data());
-  // });
+  //  con este llamado a window estamos atentos al dom 
+window.addEventListener('DOMContentLoaded', async () => {
+      // aqui hacemos llamado a la funcion getTasks donde se obtiene los datos de firebase, 
+      // se utiliza el metodo .data() para transformar los archivos firebase a archivos JavaScript
+    onGetTasks((querySnapshot) =>{
+      tasksConrainer.innerHTML = "";
 
-  onGetTasks((querySnapshot) => {
-    tasksContainer.innerHTML = "";
-
-    querySnapshot.forEach((doc) => {
-      const task = doc.data();
-
-      tasksContainer.innerHTML += `
-      <div class="card card-body mt-2 border-primary">
-    <h3 class="h5">${task.title}</h3>
-    <p>${task.description}</p>
-    <div>
-      <button class="btn btn-primary btn-delete" data-id="${doc.id}">
-        🗑 Delete
-      </button>
-      <button class="btn btn-secondary btn-edit" data-id="${doc.id}">
-        🖉 Edit
-      </button>
-    </div>
-  </div>`;
+        querySnapshot.forEach(doc => {
+          
+          const task = doc.data()
+          tasksConrainer.innerHTML +=`
+              <div class="card card-body mt-2 border-primary">
+                  <h3 class="h5">${task.title}</h3>
+                  <p >${task.description}</p>
+                  <div>
+                  <button class='btn btn-primary btn-delete' data-id="${doc.id}"> delete</button>
+                  <button class='btn btn-danger btn-update' data-id="${doc.id}"> update</button>
+                  </div>
+              </div>
+          `
     });
 
-    const btnsDelete = tasksContainer.querySelectorAll(".btn-delete");
-    btnsDelete.forEach((btn) =>
-      btn.addEventListener("click", async ({ target: { dataset } }) => {
-        try {
-          await deleteTask(dataset.id);
-        } catch (error) {
-          console.log(error);
-        }
+      const btnDelete =  tasksConrainer.querySelectorAll('.btn-delete');
+       btnDelete.forEach(btn =>{
+        btn.addEventListener('click', ({target: {dataset}}) =>{
+          deleteTasks(dataset.id);
+
+        });       
       })
-    );
 
-    const btnsEdit = tasksContainer.querySelectorAll(".btn-edit");
-    btnsEdit.forEach((btn) => {
-      btn.addEventListener("click", async (e) => {
-        try {
-          const doc = await getTask(e.target.dataset.id);
-          const task = doc.data();
-          taskForm["task-title"].value = task.title;
-          taskForm["task-description"].value = task.description;
+      const btnUpdate = tasksConrainer.querySelectorAll('.btn-update');
+      btnUpdate.forEach(btn =>{
+          btn.addEventListener('click', async ({target: {dataset}})=>{
+            const doc = await getTask(dataset.id)
+            const task = doc.data();
 
-          editStatus = true;
-          id = doc.id;
-          taskForm["btn-task-form"].innerText = "Update";
-        } catch (error) {
-          console.log(error);
-        }
-      });
-    });
+            taskForm['task-title'].value = task.title; 
+            taskForm['task-description'].value = task.description;
+
+            editStatus = true;
+            id = dataset.id;
+
+            taskForm['btn-task-form'].innerText = 'Update';
+            
+        });
+      
+      })
+
   });
+
 });
 
-taskForm.addEventListener("submit", async (e) => {
+
+
+taskForm.addEventListener('submit', (e) => {
+
   e.preventDefault();
 
-  const title = taskForm["task-title"];
-  const description = taskForm["task-description"];
+  const title = document.getElementById('task-title');
+  const description = document.getElementById('task-description');
 
-  try {
-    if (!editStatus) {
-      await saveTask(title.value, description.value);
-    } else {
-      await updateTask(id, {
-        title: title.value,
-        description: description.value,
+    if(!editStatus){
+      saveTask(title.value , description.value);
+    }else{
+      updateTask(id, {
+        title: title.value , 
+        description: description.value
       });
-
-      editStatus = false;
-      id = "";
-      taskForm["btn-task-form"].innerText = "Save";
     }
 
-    taskForm.reset();
-    title.focus();
-  } catch (error) {
-    console.log(error);
-  }
-});
+  taskForm.reset();
+  editStatus = false;
+  taskForm['btn-task-form'].innerText = 'Save';
+})
